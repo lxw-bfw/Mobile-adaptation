c0b48f10dc7f84de7a75db9a67b0390ccc5a6a98
LXY-1 <1143167344@qq.com>
2019-10-19 17:23:11 +0800

    sencod

--- rem原理理解与案例demo/vueapp/myrem-project/src/components/header.vue
+++ rem原理理解与案例demo/vueapp/myrem-project/src/components/header.vue
@@ -4,7 +4,7 @@
  * @Author: lxw
  * @Date: 2019-10-18 14:42:08
  * @LastEditors: lxw
- * @LastEditTime: 2019-10-18 14:48:47
+ * @LastEditTime: 2019-10-19 17:21:37
  -->
 <template>
   <div id="header">
@@ -18,7 +18,7 @@ export default {
   data: function() {
     return {
 
-      msg: 'hello header'
+      msg: '移动端适配--rem方案'
     }
   }
 }
